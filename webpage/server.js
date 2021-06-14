fa04080c892b2a7a9bc55509a0f2da6e140829e9
Sergio Esquina <64465478+SEPOXCRAFT@users.noreply.github.com>
2021-06-14 20:39:58 +0000

    Backup antes de los cambios

--- webpage/server.js
+++ webpage/server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express();
-const ejs = require("ejs")
-
-app.set("view engine", "ejs")
+const api = require('express').Router()
+let app = require("express")
+app = app()
+const fs = require("fs")
+const { join } = require("path");
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -16,8 +16,34 @@ app.get('/codes/create', (req, res) => {
   res.sendFile(__dirname + '/codes/create.html')
 })
 
-app.get('/codes/search', (req, res) => {
-  res.sendFile(__dirname + '/codes/search.php')
+api.get('/api/comandos', (req, res) => {
+  let cmddir = join(__dirname, "..", "bot", "comandos");
+  let cmds = [];
+
+  fs.readdirSync(require('../bot/comandos/')).forEach(dir => {
+    if(dir.endsWith('.js')){
+      cmds.push({
+        name: dir.name,
+        aliases: dir.alias,
+        usage: dir.use,
+        description: dir.description,
+        category: dir.category
+      });
+    }else{
+      const commands = fs.readdirSync(require(`../bot/comandos/${dir}`));
+      for(let file of commands){
+        if(file.endsWith('.js')){
+          cmds.push({
+            name: file.name,
+            aliases: file.alias,
+            usage: file.use,
+            description: file.description,
+            category: file.category
+          });
+        }
+      }
+    }
+  })
 })
 
 module.exports = () => {
